f293d8863bcb1e2c571bd6ff90d69ef892fcf330
John <jooke98@outlook.com>
2020-11-30 16:51:34 -0700

    add product working. needs to fix res.redirect

--- routes/api/add_product.js
+++ routes/api/add_product.js
@@ -4,11 +4,31 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.post("/", (req, res) => {
-    console.log("form data:", req.body);
+    console.log(req.body);
+    const artistId = req.body["artist-id-input"];
+    const merchId = req.body['merch-id'];
+    const merchName = req.body['merch-name-id'];
+    const merchDescription = req.body['merch-description-id'];
+    const cost = req.body.cost;
+    const size = req.body['merch-size'];
+    const thumbnailURL = req.body['merch-thumbnail-url-id'];
+    const coverURL = req.body['merch-cover-photo-url-id'];
     const query = `
-
+    INSERT INTO products (artist_id, merch_id, name, description, cost, size, thumbnail_photo_url, cover_photo_url, sold_out)
+    VALUES(${artistId}, ${merchId}, '${merchName}', '${merchDescription}', '$${cost}', '${size}', '${thumbnailURL}', '${coverURL}', false)
+    RETURNING *;
     `;
-    res.redirect("/");
+    console.log(query);
+    db.query(query)
+      .then((data) => {
+        console.log('data added', data.rows);
+        // eslint-disable-next-line no-undef
+        res.redirect("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
   });
   return router;
 };
